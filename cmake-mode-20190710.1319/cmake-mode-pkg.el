be9bb88df8e6c72810d8872f491b3d13e35546d6
soblin <hilo.soblin@gmail.com>
2020-09-02 16:02:09 +0900

    (Wed Sep  2 16:02:09 JST 2020)

---
+++ cmake-mode-20190710.1319/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20190710.1319" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "a6b379181495794d47cae256dfd94f43fbfe5d48")
---
+++ yatex-20200208.931/yatex-pkg.el
@@ -0,0 +1,4 @@
+(define-package "yatex" "20200208.931" "Yet Another tex-mode for emacs //野鳥//" 'nil :commit "f4c2dca86202c2da5b4f0f6ec97c49dd1cb01e6c")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
