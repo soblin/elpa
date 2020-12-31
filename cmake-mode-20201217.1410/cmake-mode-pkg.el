5d5c41600fa39fc3336995f95874b66fc4cefdc7
soblin <hilo.soblin@gmail.com>
2020-12-31 10:48:05 +0900

    (Thu Dec 31 10:48:05 JST 2020)

---
+++ cmake-mode-20201217.1410/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20201217.1410" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "95c59252c4116c185e898762d82b49d7a4f42445")
