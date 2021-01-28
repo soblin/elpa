858915e79cf5a674f2002d44b66ec978df4d44ab
soblin <mamoru.sobue@jpl.nasa.gov>
2021-01-28 00:59:46 -0800

    updated on 2021-01-28

---
+++ cmake-mode-20210104.1831/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/mamoru/.emacs.d/elpa/cmake-mode-20210104.1831/cmake-mode.el  -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20210104.1831" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "f2c4ea6807c29aed6453012b604788e81200584a")
