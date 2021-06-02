426fca1f6cf1050a2603d1004e3f8c215830a74a
MamoruSobue <mamoru.sobue@jpl.nasa.gov>
2021-06-01 19:58:46 -0700

    2021/05/01

--- matlab-mode-20210302.1630/matlab-mode-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "matlab-mode" "20210302.1630" "Major mode for MATLAB(R) dot-m files" 'nil :commit "a47515461ae22fcb90b6f8e45a6304bcb44b953c")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
