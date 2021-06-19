724638603edbd5cc4a4516fce51a4b23e4dc094a
MamoruSobue <mamoru.sobue@jpl.nasa.gov>
2021-06-19 09:06:36 -0700

    (2021年  6月 19日 土曜日 09:06:36 PDT)

---
+++ magit-20210619.1059/magit-pkg.el
@@ -0,0 +1,18 @@
+(define-package "magit" "20210619.1059" "A Git porcelain inside Emacs."
+  '((emacs "25.1")
+    (dash "20210330")
+    (git-commit "20210525")
+    (magit-section "20210525")
+    (transient "20210524")
+    (with-editor "20210524"))
+  :commit "d33fc279bd73de8b5969528d2e5e2a8eaeb3fdce" :authors
+  '(("Marius Vollmer" . "marius.vollmer@gmail.com")
+    ("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("git" "tools" "vc")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ magit-section-20210615.1036/magit-section-pkg.el
@@ -0,0 +1,13 @@
+(define-package "magit-section" "20210615.1036" "Sections for read-only buffers"
+  '((emacs "25.1")
+    (dash "20210330"))
+  :commit "d33fc279bd73de8b5969528d2e5e2a8eaeb3fdce" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("tools")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
