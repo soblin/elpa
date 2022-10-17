d8b86209e008081c67a9414269a799341d9d15d1
soblin <hilo.soblin@gmail.com>
2022-10-18 03:08:52 +0900

    no need to save history
    Signed-off-by: soblin <hilo.soblin@gmail.com>

---
+++ doom-modeline-20230822.1514/doom-modeline-pkg.el
@@ -0,0 +1,17 @@
+(define-package "doom-modeline" "20230822.1514" "A minimal and modern mode-line"
+  '((emacs "25.1")
+    (compat "29.1.1.1")
+    (nerd-icons "0.0.1")
+    (shrink-path "0.2.0"))
+  :commit "502e8d8e6566eb52f2dcd1c052cedc628217f6e8" :authors
+  '(("Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainers
+  '(("Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainer
+  '("Vincent Zhang" . "seagle0128@gmail.com")
+  :keywords
+  '("faces" "mode-line")
+  :url "https://github.com/seagle0128/doom-modeline")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
